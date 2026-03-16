aea9569ef9d7438a787db277598f86a9f9fcd2cc
itsrafftech <itsrafftech@gmail.com>
2026-03-16 23:11:56 +0700

    fix aos lifecycle

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import "animate.css";
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
-AOS.init();
 
 createRoot(document.getElementById('root')).render(
 <StrictMode>
